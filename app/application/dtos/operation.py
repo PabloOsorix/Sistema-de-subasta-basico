53ac034f144a241652ec4bc21b79dd40c16585fa
PabloOsorix <osorixjuan@gmail.com>
2024-09-18 11:56:14 -0500

    💡 Remove old commentary

--- app/application/dtos/operation.py
+++ app/application/dtos/operation.py
@@ -13,9 +13,6 @@ class OperationResponseDTO(BaseModel):
     user_id: str
     amount: int
     description: str
-    #Tome esta decision ya que el estimado de ofertas por operacion
-    #no sera demasiado alto, podemos calcular y enviar el monto disponible
-    #para que no deba realizarse desde el front-end el calculo.
     available_amount: int | str
     interest_rate: float
     limit_date: str
