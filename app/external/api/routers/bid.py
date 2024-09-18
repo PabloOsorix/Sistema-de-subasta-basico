1895c65c9c669452f9f234ebdc46b01674571a70
PabloOsorix <osorixjuan@gmail.com>
2024-09-18 11:53:06 -0500

    ♻️ Agroup common repositories and remove user verification
     I also add new dependency verify user in the APIRouter,  delegating user verification to it and removing duplicate code from each endpoint

--- app/external/api/routers/bid.py
+++ app/external/api/routers/bid.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from app.core.factories.bid_builder import BidBuilder
 from app.application.interfaces.bid_service import BidService
 from app.application.dtos.bid import BidCreateDTO
-from app.external.auth.auth_service import get_current_user
+from app.external.auth.auth_service import get_current_user, verify_user
 from app.external.api.dependencies import (
     get_json_user_repository,
     get_json_operation_repository,
@@ -24,58 +24,31 @@ from pydantic import EmailStr
 
 router = APIRouter(dependencies=[Depends(get_json_user_repository),
                                  Depends(get_json_bid_repository),
-                                 Depends(get_json_operation_repository)],
+                                 Depends(get_json_operation_repository),
+                                 Depends(get_current_user),
+                                 Depends(verify_user)],
                    prefix="/bid")
 
+def repositories(operation_repository: JsonOperationRepository = Depends(get_json_operation_repository),
+                      user_repository: JsonUserRepository = Depends(get_json_user_repository),
+                      bid_repository: JsonBidRepository = Depends(get_json_bid_repository)):
+    return {"operation": operation_repository, "user": user_repository, "bid": bid_repository}
 
-@router.get("/", response_model=Union[BidOut, List[BidOut]])
-def get_all_bids_by_user(user_repository: JsonUserRepository = Depends(get_json_user_repository),
-                         operation_repository: JsonOperationRepository = Depends(
-                             get_json_operation_repository),
-                         bid_repository: JsonBidRepository = Depends(
-                             get_json_bid_repository),
-                         current_user: dict = Depends(get_current_user),
-                         event_manager: EventManager = Depends(get_event_manager)):
+repositoriesDep = Annotated[dict, Depends(repositories)]
 
-    try:
 
-        user_email = current_user.get("sub")
-        if user_email is None:
-            raise CredentialsException()
-
-        bid_service = BidService(
-            bid_repository, user_repository, operation_repository, event_manager)
-        operations = bid_service.get_all_bids_by_user(user_email)
-        return operations
-
-    except Exception as error:
-        return JSONResponse(
-            content=jsonable_encoder({"error", f"{error}"}),
-            status_code=status.HTTP_400_BAD_REQUEST,
-        )
 
 
 @router.post("/create", response_model=BidOut)
-def create_bid(bid_to_create: BidInput,
-               user_repository: JsonUserRepository = Depends(
-                   get_json_user_repository),
-               operation_repository: JsonOperationRepository = Depends(
-                   get_json_operation_repository),
-               bid_repository: JsonBidRepository = Depends(
-                   get_json_bid_repository),
-               current_user: dict = Depends(get_current_user),
+def create_bid(bid_to_create: BidInput, repositories: repositoriesDep,
                event_manager: EventManager = Depends(get_event_manager)):
 
     try:
 
-        user_email = current_user.get("sub")
-        if user_email is None:
-            raise CredentialsException()
-
         bid_builder = BidBuilder()
         new_bid = BidCreateDTO.model_construct(**bid_to_create.model_dump())
         bid_service = BidService(
-            bid_repository, user_repository, operation_repository, event_manager)
+            repositories["bid"], repositories["user"], repositories["operation"], event_manager)
         created_bid = bid_service.create_bid(new_bid, bid_builder)
         return created_bid
 
@@ -86,28 +59,39 @@ def create_bid(bid_to_create: BidInput,
         )
 
 
-@router.get("/{operation_id}", response_model=Union[BidOut, List[BidOut]])
-def get_bids_by_operation_id(operation_id: str, user_repository: JsonUserRepository = Depends(get_json_user_repository),
-                             operation_repository: JsonOperationRepository = Depends(
-                                 get_json_operation_repository),
-                             bid_repository: JsonBidRepository = Depends(
-                                 get_json_bid_repository),
-                             current_user: dict = Depends(get_current_user),
-                             event_manager: EventManager = Depends(get_event_manager)):
+@router.get("/", response_model=Union[BidOut, List[BidOut]])
+def get_all_bids_by_user(repositories: repositoriesDep, event_manager: EventManager = Depends(get_event_manager),
+                         current_user_email: str = Depends(verify_user)):
 
     try:
 
-        user_email: EmailStr = current_user.get("sub")
-        if user_email is None:
-            raise CredentialsException()
+        bid_service = BidService(
+            repositories["bid"], repositories["user"], repositories["operation"], event_manager)
+        operations = bid_service.get_all_bids_by_user(email=current_user_email)
+        return operations
 
-        bid_service = BidService(bid_repository=bid_repository,
-                                 user_repository=user_repository,
-                                 operation_repository=operation_repository,
+    except Exception as error:
+        return JSONResponse(
+            content=jsonable_encoder({"error", f"{error}"}),
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+
+@router.get("/{operation_id}", response_model=Union[BidOut, List[BidOut]])
+def get_bids_by_operation_id(operation_id: str, repositories: repositoriesDep,
+                             event_manager: EventManager = Depends(get_event_manager),
+                             current_user_email: str = Depends(verify_user)):
+
+    try:
+
+        bid_service = BidService(bid_repository=repositories["bid"],
+                                 user_repository=repositories["user"],
+                                 operation_repository=repositories["operation"],
                                  event_manager=event_manager
                                  )
         operations = bid_service.get_bids_by_operation_id(
-            operation_id, user_email)
+            operation_id, current_user_email)
         return operations
 
     except Exception as error:
