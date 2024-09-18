943e0649d42e9f82e3b546b3a22c5ead3970382c
PabloOsorix <osorixjuan@gmail.com>
2024-09-18 11:47:53 -0500

    ✏️ Fix parameters in schema model_config

--- app/external/api/schemas/bid.py
+++ app/external/api/schemas/bid.py
@@ -18,11 +18,10 @@ class BidInput(BidBase):
         "json_schema_extra": {
             "examples": [
                 {
-                    "operation_id": "bf874462-2ac1-4a98-bcae-ca92ac5e4861",
                     "user_id": "dbef75c2-0966-4f5f-8824-55f4836ce179",
+                    "operation_id": "bf874462-2ac1-4a98-bcae-ca92ac5e4861",
                     "amount": "100000",
                     "interest_rate": "5.2",
-                    "limit_date": "YYYY-MM-DD"
                 }
             ]
         }
